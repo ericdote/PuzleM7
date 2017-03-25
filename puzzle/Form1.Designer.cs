b26d13ef9eebd171659c4ce55c41400e3c7a1065
Eric <rovx15@gmail.com>
2017-03-25 16:43:50 +0100

    Agregar archivos de proyecto.

---
+++ puzzle/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(23, 20);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(457, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 55);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(457, 78);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(457, 100);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(227, 368);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 403);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ puzzle/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle
+{
+    public partial class Form1 : Form
+    {
+        private int x = 0;
+        private int y = 0;
+        private int x2, y2;
+        private int PosIniciaTop = 0;
+        private int PosIniciaLeft = 0;
+        private Image[] img; //Array de Imagenes para el puzzle
+        private PictureBox[] pb; //Array de PictureBox donde se añaden las imagenes desordenadas
+        private PictureBox imgSelect; //PictureBox de la imagen seleccionada ene se momento
+        private Boolean[] imgRepe; //Array de booleanos que controla las posiciones que ya se han añadido del puzzle
+        int cambioPosicionTop, cambioPosicionLeft, tagAux; //Variables auxiliares
+        private Image aux; //Variable para imagen auxiliar
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            //Inicializamos las imagenes
+            img = new Image[9] { Properties.Resources._1, Properties.Resources._2, Properties.Resources._3, Properties.Resources._4, Properties.Resources._5, Properties.Resources._6, Properties.Resources._7, Properties.Resources._8, Properties.Resources._9 };
+            //Inicializamos el array de imagenes para el puzle
+            pb = new PictureBox[9];
+            //Inicializamos el array de boleanos
+            imgRepe = new Boolean[9];
+            //Asignamos un tag a cada pieza del puzle, para mas adelante controlar si esta completo.
+            for (int i = 0; i < img.Length; i++)
+            {
+                img[i].Tag = i;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            imgSelect = (PictureBox)sender;
+            x = e.X;
+            y = e.Y;
+
+            PosIniciaTop = imgSelect.Top;
+            PosIniciaLeft = imgSelect.Left;
+
+            x2 = imgSelect.Top; //Le asignamos la nueva posicion a variables nuevas, ya que si van en las antiguas tenemos problemas de desplazamiento
+            y2 = imgSelect.Left;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            imgSelect = (PictureBox)sender;
+            imgSelect.BringToFront();
+            label1.Text = Convert.ToString(x) + "-" + Convert.ToString(y);
+            label2.Text = Convert.ToString(e.X) + "-" + Convert.ToString(e.Y);
+            label3.Text = Convert.ToString(PosIniciaTop) + "-" + Convert.ToString(PosIniciaLeft);
+            label4.Text = Convert.ToString(imgSelect.Top) + "-" + Convert.ToString(imgSelect.Left);
+
+            cambioPosicionTop = imgSelect.Top; //Se actualiza la posicion por donde desplazamos la imagen verticalmente
+            cambioPosicionLeft = imgSelect.Left; //Y horizontalmente
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                imgSelect.Left = (imgSelect.Left + e.X) - x; 
+                imgSelect.Top = (imgSelect.Top + e.Y) - y;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            imgSelect = (PictureBox)sender;
+            label1.Text = Convert.ToString(x) + "-" + Convert.ToString(x);
+            label2.Text = Convert.ToString(e.X) + "-" + Convert.ToString(e.X);
+            label3.Text = Convert.ToString(PosIniciaTop) + "-" + Convert.ToString(PosIniciaLeft);
+
+            imgSelect.Left = PosIniciaLeft;
+            imgSelect.Top = PosIniciaTop;
+            Point pointImg1 = new Point(x2 / 100, y2 / 100);
+            Point pointImg2 = new Point(cambioPosicionTop / 100, cambioPosicionLeft / 100);
+            foreach (var i in pb)
+            {                
+                if(((i.Location.X / 100) == pointImg2.Y)&&((i.Location.Y /100) == pointImg2.X)) //Comprovamos la posicion donde esta la imagen que arrastramos
+                {
+                    foreach (var j in pb) 
+                    {
+                        if(((j.Location.X / 100) == pointImg1.Y) && (j.Location.Y /100) == pointImg1.X)
+                        { //Comprovamos la posicion donde vamos a cambiar
+                            aux = j.Image; //Cogemos la imagen secundaria y la ponemos en el auxiliar
+                            j.Image = i.Image; //Cambiamos la secundaria por la que estamos arrastrando
+                            i.Image = aux; //Cambiamos la posicion de donde hemos arrastrado la imagen arrastrada por la imagen que habia donde esta la nueva (osea que ponemos la auxiliar aqui)
+
+                            //Lo mismo con el TAG
+                            tagAux = (int)j.Tag;
+                            j.Tag = i.Tag;
+                            i.Tag = tagAux;
+                        }
+                    }
+                    break;
+                }
+            }
+            comprovacionGanar();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Con estas variables controlamos cuantas imagenes hay por columna y fila
+            int imagenesDentroX = 0, imagenesDentroY = 0;
+            //Rellenamos el array de PicturesBox con piezas de puzzle
+            for (int i = 0; i < 9; i++)
+            {
+                //Declaramos un random para seleccionar piezas del puzzle
+                Random rnd = new Random();
+                int imgRnd = rnd.Next(0, 9);
+                //Si la imagen no ha sido escogida antes(lo comprovamos con el array de boleanos)
+                if (!imgRepe[imgRnd])
+                {
+                    //Inicializamos la casilla con un nuevo PictureBox
+                    pb[i] = new PictureBox();
+                    imgRepe[imgRnd] = true; //Asignamos la posicion en el array de booleanos a true
+                    pb[i].Image = img[imgRnd]; //Asignamos la imagen
+                    pb[i].Height = 100; //Le damos un tamaño
+                    pb[i].Width = 100;
+                    pb[i].SizeMode = PictureBoxSizeMode.StretchImage; //Le ajustamos su modo (en este caso que se ajuste a las proporciones)
+                    //Si la imagen supera los 300 px 
+                    if (imagenesDentroX >= 300)
+                    {
+                        //La X se reinicia y se baja una fila
+                        imagenesDentroX = 0;
+                        imagenesDentroY += 100;
+                    }
+                    //Si la Y se supera se reinicia la X a la posicion 0
+                    if (imagenesDentroY >= 300)
+                    {
+                        imagenesDentroX = 0;
+                    }
+                    pb[i].Location = new Point(imagenesDentroX, imagenesDentroY); //Asignamos un puntero a la imagen para poder después cogerlo
+                    imagenesDentroX += 100; //Asignamos +100 tras cada imagen añadida
+                    this.panel1.Controls.Add(pb[i]); //Le añadimos a la imagen funciones
+                    pb[i].MouseMove += pictureBox1_MouseMove;
+                    pb[i].MouseUp += pictureBox1_MouseUp;
+                    pb[i].MouseDown += pictureBox1_MouseDown;
+                    pb[i].Tag = img[imgRnd].Tag; //Le asignamos un TAG a la imagen
+
+                }
+                else
+                {
+                    i--; //Con tal de no dejarnos ningun hueco vacio en el puzzle, restamos uno a i, para asi que hasta que el puzzle no este completo no deje de incluir imagenes, ya que si se repite pararia antes de estar lleno entero.
+                }
+
+            }
+            btnStart.Enabled = false; //Desactivamos el boton de iniciar partida para que no pete al intentar inicar varias veces seguidas.
+        }
+        private void comprovacionGanar()
+        {
+            //Boolean iniciado a falso para comprovar si hemos ganado
+            Boolean comprovacio = false;
+            //Recorre el array de Picturebox
+            for (int i = 0; i < pb.Length; i++)
+            {
+                int siguiente = i + 1;
+                if((int)pb[i].Tag+1 != siguiente)
+                {
+                    comprovacio = false;
+                    break;
+                } else
+                {
+                    comprovacio = true;
+                }
+            }
+            //Si todas las imagenes se han colocado correctamente salta el siguiente mensaje, si no, no sale.
+            if (comprovacio)
+                if (comprovacio)
+            {
+                MessageBox.Show("HAS GANADO!");
+            }
+        }
+    }
+}
